b1f97c09ed8bd0e8f7435224ed358b432d2fbc45
kartavya79 <kartavyagautam6@gmail.com>
2026-09-21 13:18:46 +0530

    minnor update

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ const app = express()
 const port = 3000
 app.use(express.json());
 
+
 app.get('/',(req,res)=>{
     res.send("hello wold!")
 })
 
 let users=[
-    {id:1,name:"Alice",email:"alice@example.com"},
-    {id:2,name:"Bob",email:"bob@example.com"},
-    {id:3,name:"Charlie",email:"charlie@example.com"},
-    {id:4,name:"David",email:"david@example.com"},
+    {id:1,name:"Krish",email:"krish@example.com"},
+    {id:2,name:"Kartavya",email:"Kartavya@example.com"},
 ];
 
 //get all users
